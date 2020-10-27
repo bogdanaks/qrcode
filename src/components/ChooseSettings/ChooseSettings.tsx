90996ca4a61e591fc1d76e2aaf317554a16cb825
bogdanaks <bogdanaks@bk.ru>
2020-10-27 23:48:26 +0300

    Change ChooseSettings component

--- src/components/ChooseSettings/ChooseSettings.tsx
+++ src/components/ChooseSettings/ChooseSettings.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
 import transparency from '../../assets/transparency.svg'
+import qrImg from '../../assets/qrImg.png'
 
 import styles from './styles.module.scss'
 import { BorderSlider } from '../BorderSlider/BorderSlider'
+import { QRSizeSlider } from '../QRSizeSlider/QRSizeSlider'
 
 interface ChooseSettingsProps {
+    qrSizeValue: string
+    setQrSizeValue: (str: string) => void
     borderValue: string
     setBorderValue: (str: string) => void
+    radiusValue: string
+    setRadiusValue: (str: string) => void
     isShowText: boolean
     setIsShowText: (toggle: boolean) => void
     isShowBg: boolean
@@ -15,29 +21,53 @@ interface ChooseSettingsProps {
 }
 
 export const ChooseSettings: React.FC<ChooseSettingsProps> = ({
+    qrSizeValue,
+    setQrSizeValue,
     borderValue,
     setBorderValue,
+    radiusValue,
+    setRadiusValue,
     isShowText,
     setIsShowText,
     isShowBg,
     setIsShowBg,
 }) => {
     const [showBorder, setShowBorder] = React.useState<boolean>(false)
+    const [showQRSize, setQRSize] = React.useState<boolean>(false)
 
-    const handleShowBorderSet = () => {
+    const handleShowBorderPopUp = () => {
+        if (showQRSize) {
+            setQRSize(false)
+        }
         setShowBorder(!showBorder)
+    }
+
+    const handleShowQRSizePopUp = () => {
+        if (showBorder) {
+            setShowBorder(false)
+        }
+        setQRSize(!showQRSize)
     }
 
     return (
         <div className={styles.chooseBlock}>
             <ul>
                 <li>
-                    <button className={styles.borderSet} onClick={handleShowBorderSet}></button>
+                    <button className={styles.borderSet} onClick={handleShowBorderPopUp}></button>
                     <div
                         className={[
                             styles.activeLine,
                             Number(borderValue) > 0 && styles.showLine,
                         ].join(' ')}></div>
+                </li>
+                <li>
+                    <button className={styles.qr} onClick={handleShowQRSizePopUp}>
+                        <img src={qrImg} alt="QRImg" />
+                    </button>
+                    <div
+                        className={[styles.activeLine, showQRSize && styles.showLine].join(
+                            ' ',
+                        )}></div>
                 </li>
                 <li>
                     <button className={styles.transparency} onClick={() => setIsShowBg(!isShowBg)}>
@@ -58,7 +88,15 @@ export const ChooseSettings: React.FC<ChooseSettingsProps> = ({
                         )}></div>
                 </li>
             </ul>
-            {showBorder && <BorderSlider value={borderValue} setValue={setBorderValue} />}
+            {showBorder && (
+                <BorderSlider
+                    value={borderValue}
+                    setValue={setBorderValue}
+                    radiusValue={radiusValue}
+                    setRadiusValue={setRadiusValue}
+                />
+            )}
+            {showQRSize && <QRSizeSlider value={qrSizeValue} setValue={setQrSizeValue} />}
         </div>
     )
 }

c5d2cda1d5047e81e7c0d20cefae83366196faae
bogdanaks <bogdanaks@bk.ru>
2020-10-27 22:49:27 +0300

    Import components

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,21 +4,44 @@ import { RGBColor } from 'react-color'
 import styles from './styles.module.scss'
 
 import { ColorPicker } from '../ColorPicker/ColorPicker'
+import { QRBlock } from '../QRBlock/QRBlock'
+import { InputLink } from '../InputLink/InputLink'
+import { ChooseSettings } from '../ChooseSettings/ChooseSettings'
 
 export const Main: React.FC = () => {
-    const [color, setColor] = React.useState<{ hex: string; rgb: RGBColor }>({
+    const [color, setColor] = React.useState<{ hex: string; rgba: RGBColor }>({
         hex: '#fff',
-        rgb: { r: 51, g: 51, b: 51 },
+        rgba: { r: 51, g: 51, b: 51, a: 1 },
     })
+    const [inputValue, setInputValue] = React.useState<string>('vk.com')
+    const [borderValue, setBorderValue] = React.useState<string>('2')
+    const [isShowBg, setIsShowBg] = React.useState<boolean>(false)
+    const [isShowText, setIsShowText] = React.useState<boolean>(false)
+
     return (
         <div className={styles.main}>
             <h2>Just QR Code</h2>
-            <div
-                className={styles.colorBlock}
-                style={{
-                    backgroundColor: `rgba(${color.rgb.r},${color.rgb.g},${color.rgb.b},${color.rgb.a})`,
-                }}></div>
-            <ColorPicker color={color} setColor={setColor} />
+            <div className={styles.container}>
+                <QRBlock
+                    fgColor={color.rgba}
+                    bgColor={isShowBg ? 'transparent' : 'white'}
+                    value={inputValue}
+                    borderSize={Number(borderValue)}
+                    isShowText={isShowText}
+                />
+                <div className={styles.settingsBlock}>
+                    <InputLink value={inputValue} setValue={setInputValue} />
+                    <ColorPicker color={color} setColor={setColor} />
+                    <ChooseSettings
+                        borderValue={borderValue}
+                        setBorderValue={setBorderValue}
+                        isShowText={isShowText}
+                        setIsShowText={setIsShowText}
+                        isShowBg={isShowBg}
+                        setIsShowBg={setIsShowBg}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
